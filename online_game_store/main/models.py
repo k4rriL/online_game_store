8f9115b4037bca94c23dd4956d228d8ed6c03dfd
Karri Lehtiranta <lehtirk1@aalto.fi>
2017-01-04 15:21:55 +0200

    Initial commit and model definitions

---
+++ online_game_store/main/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Game(models.Model):
+    id = models.PositiveIntegerField(unique = True, primary_key = True)
+    name = models.CharField(max_length = 100, unique = True)
+    description = models.TextField()
+    price = models.FloatField()
+    purchaseCount = models.PositiveIntegerField()
+    developerEmail = models.ForeignKey('Developer', related_name='games')
+
+    CATEGORIES = (
+        ('SPO', 'Sports'),
+        ('RAC', 'Racing'),
+        ('RPG', 'RPG'),
+        ('ACT', 'Action'),
+        ('ADV', 'Adventure'),
+        ('CAS', 'Casual'),
+        ('STR', 'Strategy'),
+        ('OTH', 'Other')
+    )
+
+    category = models.CharField(max_length = 3, choices = CATEGORIES)
+    class Meta:
+        ordering = ("purchaseCount",)
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+class Developer(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+class GamesOfPlayer(models.Model):
+    gameID = models.ForeignKey('Game')
+    username = models.ForeignKey('Player', related_name = 'games')
+    highscore = models.PositiveIntegerField()
+    gameState = models.TextField()
