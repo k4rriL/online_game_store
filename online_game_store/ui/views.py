b90f9dceb09d20de589e10aa3c8214068445720d
Karri Lehtiranta <lehtirk1@aalto.fi>
2017-01-14 20:39:15 +0200

    Added games view, some fucntionality missing

--- online_game_store/ui/views.py
+++ online_game_store/ui/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from gamedata.models import Game, Player, GamesOfPlayer, Developer
+from gamedata.models import Game, Player, GamesOfPlayer
 from django.contrib.auth.models import User
 from hashlib import md5
-from django.http import HttpResponseRedirect
-from random import randint
-import time
-from . import forms
 
 # Create your views here.
 
@@ -13,20 +9,17 @@ def front(request):
     context = {}
     return render(request, "ui/index.html", context)
 
-#function for displaying correct information about
-#wanted game and creating the correct values for
-#the payment service
 def game_info(request):
     context = {}
     #Test game
-    game = get_object_or_404(Game, id=1234234456)
+    game = get_object_or_404(Game, id=1234)
     context["game"] = game
     #Test user
     test_user = get_object_or_404(User, username="My Testuser")
     player = get_object_or_404(Player, user=test_user)
     context["player"] = player
-    pid = str(game.id) + player.user.username.replace(" ", "")
 
+    pid = str(game.id) + player.user.username.replace(" ", "")
     sid = "OnlineGameStore"
     token = "5fa6f9b7ea1628e4d373b4003bce9eb5"
     amount = game.price
@@ -39,7 +32,6 @@ def game_info(request):
     return render(request, "ui/showgame.html", context)
 
 
-#function for handling successful payment
 def game_purchase_success(request):
 
     context = {}
@@ -48,23 +40,19 @@ def game_purchase_success(request):
     ref = request.GET["ref"]
     result = request.GET["result"]
     checksum = request.GET["checksum"]
-
     game = get_object_or_404(Game, id=game_id)
     #Test user
     test_user = get_object_or_404(User, username="My Testuser")
     player = get_object_or_404(Player, user=test_user)
     secret_key = "5fa6f9b7ea1628e4d373b4003bce9eb5"
-
     checksumstr = "pid={}&ref={}&result={}&token={}".format(pid, ref, result, secret_key)
     correct_checksum = calculateHash(checksumstr)
-
     if request.GET["result"] == "success" and correct_checksum == checksum:
-        new_relation = GamesOfPlayer.objects.create(game=game, user=player, highscore=0, gameState="Ready")
+        new_relation = GamesOfPlayer.objects.create(gameID=game, username=player, highscore=0, gameState="Ready")
         new_relation.save()
 
     #TODO update this to redirect to game
-    return HttpResponseRedirect("/")
-
+    return front(request)
 
 def add_new_game(request):
 
@@ -113,8 +101,6 @@ def add_new_game(request):
     #now also including the newly added game?
     return render(request, "ui/index.html", context)
 
-
-#Function for calculating hash value from string
 def calculateHash(str):
     m = md5()
     m.update(str.encode("ascii"))
